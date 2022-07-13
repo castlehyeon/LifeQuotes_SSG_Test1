6f11e0da64b32d3843d6a27e962c1578cbb30921
castlehyeon <qnftktls118@naver.com>
2022-07-13 13:28:28 +0900

    개발환경세팅

---
+++ src/main/java/com/ll/LifeQuotes_SSG_Test1/App.java
@@ -0,0 +1,7 @@
+package com.ll.LifeQuotes_SSG_Test1;
+
+public class App {
+    public void run() {
+        System.out.println("== 명언 SSG ==");
+    }
+}
---
+++ src/main/java/com/ll/LifeQuotes_SSG_Test1/Main.java
@@ -0,0 +1,7 @@
+package com.ll.LifeQuotes_SSG_Test1;
+
+public class Main {
+    public static void main(String[] args) {
+        new App().run();
+    }
+}
