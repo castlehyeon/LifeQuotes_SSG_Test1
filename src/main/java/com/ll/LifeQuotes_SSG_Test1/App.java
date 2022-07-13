ecc8bab9ef814b3b288fce6ce8b977dd4b0eac27
castlehyeon <qnftktls118@naver.com>
2022-07-13 13:45:50 +0900

    등록과 글번호 증가 구현

--- src/main/java/com/ll/LifeQuotes_SSG_Test1/App.java
+++ src/main/java/com/ll/LifeQuotes_SSG_Test1/App.java
@@ -3,6 +3,8 @@ package com.ll.LifeQuotes_SSG_Test1;
 import java.util.Scanner;
 
 public class App {
+    private int wiseSayingId=0;
+
     public void run() {
         System.out.println("== 명언 SSG ==");
 
@@ -18,6 +20,11 @@ public class App {
                     String wiseSaying = sc.nextLine().trim();
                     System.out.println("작가 : ");
                     String author = sc.nextLine().trim();
+                    ++wiseSayingId;
+                    System.out.printf("%d번째 명언이 등록 되었습니다.\n", wiseSayingId);
+                    break;
+                case "목록":
+                    break outer;
                 case "종료":
                     break outer;
             }
