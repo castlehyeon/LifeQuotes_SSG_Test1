83a6e7033226c59d76b20f334deaca302aff0e9e
castlehyeon <qnftktls118@naver.com>
2022-07-13 13:39:06 +0900

    명령 프롬프트와 종료기능 구현

--- src/main/java/com/ll/LifeQuotes_SSG_Test1/App.java
+++ src/main/java/com/ll/LifeQuotes_SSG_Test1/App.java
@@ -1,7 +1,28 @@
 package com.ll.LifeQuotes_SSG_Test1;
 
+import java.util.Scanner;
+
 public class App {
     public void run() {
         System.out.println("== 명언 SSG ==");
+
+        Scanner sc = new Scanner(System.in);
+        outer:
+        while ( true ) {
+            System.out.println("명령) ");
+            String cmd = sc.nextLine().trim();
+
+            switch (cmd){
+                case "등록":
+                    System.out.println("명언 : ");
+                    String wiseSaying = sc.nextLine().trim();
+                    System.out.println("작가 : ");
+                    String author = sc.nextLine().trim();
+                case "종료":
+                    break outer;
+            }
+        }
+
+        sc.close();
     }
 }
