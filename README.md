42ae47e0e4f9f806a5e0a738279095b38a0a1935
castlehyeon <qnftktls118@naver.com>
2022-07-17 16:05:45 +0900

    issue

--- README.md
+++ README.md
@@ -9,5 +9,18 @@
 4차-2: *객체를 JSON으로 변환하여 저장, 불러오기 구현 TDD* <br>
 4차-3: ~~*빌드 기능 구현*~~ <br><br>
 
-*해시맵 도입X
-*JSON변환 X
+*해시맵 도입X <br>
+*JSON변환 X <br><br>
+
+## 이슈
+
+수정과 삭제 시
+
+1. 목록 번호보다 넘어서면 IndexOutOfBoundsException 오류<br>
+
+2.  id값에 아무것도 적지 않는다면 NumberFormatException 오류 <br>
+
+3. 삭제 또는 수정만 적는다면 ArrayIndexOutOfBoundsException 오류 <br>
+
+4. 삭제 또는 수정 뒤 ?까지만 적는다면 ArrayIndexOutOfBoundsException 오류
+
--- src/test/java/com/ll/LifeQuotes_SSG_Test1/Apptest.java
+++ src/test/java/com/ll/LifeQuotes_SSG_Test1/Apptest.java
@@ -2,6 +2,8 @@ package com.ll.LifeQuotes_SSG_Test1;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Apptest {
@@ -27,5 +29,19 @@ public class Apptest {
         String line = Util.loadFile("test_data/1.json");
 
         assertEquals("내용", line);
+    }
+    @Test
+    void 삭제기능(){
+        Rq rq = new Rq("삭제?=55");
+        ArrayList<WiseSaying> wiseSayings = new ArrayList<>();
+        int deleteId = rq.getIntParam("id", 0);
+        String message = "명언은 존재하지 않습니다.";
+        if ( deleteId == 0 ){ //deleteId값이 사용자 번호(index+1)
+            System.out.printf(message);
+        } else {
+            wiseSayings.remove(deleteId-1);
+            System.out.printf("%d번째 명언이 삭제 되었습니다.\n", deleteId);
+        }
+        assertEquals("명언은 존재하지 않습니다.", message);
     }
 }
